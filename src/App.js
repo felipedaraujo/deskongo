7d11a6eb1622f008cc50ff7a4f206d759a375864
Felipe de Araujo <felipe.araujo@livongo.com>
2019-04-11 08:54:00 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import floorplan from './floor-plan.svg';
+import { fabric } from 'fabric';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    const canvas = new fabric.Canvas('canvas');
+
+    fabric.loadSVGFromURL(floorplan, (objects, options) => {
+      console.log('[OBJECTS]', objects);
+
+      objects.forEach(object => canvas.add(object));
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <canvas ref="canvas" id="canvas" width="1955.56px" height="1511.11px" />
       </div>
     );
   }
